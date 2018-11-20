2276bb03b248508b69ccd462e204f44a05e9bd1c
Kiri Tavaga <kiri.tavaga@icloud.com>
2018-11-21 11:42:56 +1300

    created input form to show post links

--- client/components/LoadSubreddit.jsx
+++ client/components/LoadSubreddit.jsx
@@ -2,10 +2,28 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {fetchPosts} from '../actions'
 
-const LoadSubreddit = ({dispatch}) => (
-  <button onClick={() => dispatch(fetchPosts('newzealand'))}>
-    Fetch Posts
-  </button>
-)
+class LoadSubreddit extends React.Component {
+    state = {
+      subreddit: ''
+    }
+
+  
+  changeHandler = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  render () {
+    return (
+      <div>
+        <input type="text" name='subreddit' value={this.state.subreddit} onChange={this.changeHandler} />
+        <button onClick={() => this.props.dispatch(fetchPosts(this.state.subreddit))}>
+          Fetch Posts
+        </button>
+      </div>
+    )
+  }
+}
 
 export default connect()(LoadSubreddit)
