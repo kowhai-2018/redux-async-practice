789fe299b7c5128a657c273c5efdeb35f09da61a
Carel Jan Starreveld <carelstarreveld@hotmail.com>
2018-11-21 11:11:10 +1300

    added date, content and url to posts

--- client/components/Post.jsx
+++ client/components/Post.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 
-const Post = ({title}) => (
-  <div>{title}</div>
+const Post = ({title, date, content, url}) => (
+  <div>
+    <h5><a href={url}>{date} - {title}</a> </h5>
+    <br/>
+    <p>
+      {content}
+    </p>
+  </div>
 )
 
 export default Post
