6ebeec75b3dd9112dba18ce081dec68182ed0b52
Alina Kireeva <alina.kireeva88@gmail.com>
2018-11-21 11:42:50 +1300

    made LoadSubreddit work

--- client/components/LoadSubreddit.jsx
+++ client/components/LoadSubreddit.jsx
@@ -2,10 +2,36 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {fetchPosts} from '../actions'
 
-const LoadSubreddit = ({dispatch}) => (
-  <button onClick={() => dispatch(fetchPosts('newzealand'))}>
-    Fetch Posts
-  </button>
-)
+class LoadSubreddit extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {value: ''}
 
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange (event) {
+    this.setState({value: event.target.value})
+  }
+
+  handleSubmit (event) {
+    alert('A name was submitted: ' + this.state.value)
+    event.preventDefault()
+  }
+
+  render () {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+  Subreddit
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+          <button onClick={() => this.props.dispatch(fetchPosts(this.state.value))}>
+    Fetch Posts
+          </button>
+        </label>
+      </form>
+    )
+  }
+}
 export default connect()(LoadSubreddit)
