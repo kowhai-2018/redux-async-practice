a09b849c284bc7409acc2f659cb5e4aab487c715
Carel Jan Starreveld <carelstarreveld@hotmail.com>
2018-11-21 11:39:15 +1300

    created formfield so that we can open other subreddits

--- client/components/LoadSubreddit.jsx
+++ client/components/LoadSubreddit.jsx
@@ -2,10 +2,28 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {fetchPosts} from '../actions'
 
-const LoadSubreddit = ({dispatch}) => (
-  <button onClick={() => dispatch(fetchPosts('newzealand'))}>
-    Fetch Posts
-  </button>
-)
+class LoadSubreddit extends React.Component {
+  state = {
+    subreddit : 'newzealand',
+    countryForm : ''
+  }
+
+  onChangeHandler = e => {
+    this.setState({
+      countryForm: e.target.value
+    })
+  }
+
+  render () {
+    return (
+      <React.Fragment>
+        <input type='text' placeholder='Type the name of the subreddit' value={this.state.countryForm} onChange={this.onChangeHandler} />
+        <button onClick={() => this.props.dispatch(fetchPosts(this.state.countryForm))}>
+          Fetch Posts
+        </button>
+      </React.Fragment>
+    )
+  }
+}
 
 export default connect()(LoadSubreddit)
