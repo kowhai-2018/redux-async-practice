97692548cb8884e9f199562c6bb6cf3a5b807f55
Chris Laupama <chrislaupama@icloud.com>
2018-11-21 14:52:37 +1300

    removed api from class and used redux to call api data

--- client/components/Joke.jsx
+++ client/components/Joke.jsx
@@ -1,30 +1,14 @@
 import React from 'react'
-import request from 'superagent'
+import {connect} from 'react-redux'
+import {fetchJoke} from '../actions'
 
-export default class Joke extends React.Component {
-  componentDidMount () {
-    this.getJoke()
-  }
-  state = {
-    joke: ''
-  }
-
-  getJoke () {
-    request
-  .get('https://geek-jokes.sameerkumar.website/api')
-  .then(res => {
-    this.setState({
-      joke: res.body
-    })
-  })
-  .catch(err => {
-    console.error(err.message)
-  })
-  }
+class Joke extends React.Component {
 
   render () {
     return (
-      <h2>{this.state.joke}</h2>
+      <button onClick={() => this.props.dispatch(fetchJoke())}>Fetch Joke</button>
     )
   }
 }
+
+export default connect()(Joke)
