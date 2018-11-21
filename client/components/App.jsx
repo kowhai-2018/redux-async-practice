14791bfdbd8714e1df672102faa9b227939b0726
Chris Laupama <chrislaupama@icloud.com>
2018-11-21 14:28:34 +1300

    imported joke component and placed within app

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -4,10 +4,12 @@ import ErrorMessage from './ErrorMessage'
 import LoadSubreddit from './LoadSubreddit'
 import SubredditList from './SubredditList'
 import WaitIndicator from './WaitIndicator'
+import Joke from './Joke'
 
 const App = () => (
   <div className='app'>
     <ErrorMessage />
+    <Joke />
     <LoadSubreddit />
     <WaitIndicator />
     <SubredditList />
